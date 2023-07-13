e7b16bc84883ca47d48f5dc84ece7305f9165a5c
kiprotich01 <kiprotich.bn@gmail.com>
2023-07-13 00:26:35 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,10 @@
 
 /**
  * main - Entry point of the program
- *
- * Description: Prints the message "Programming is like building a multilingual puzzle"
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
